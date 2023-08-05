8d406a4a0a94d7e0e55e7c33ccefdc77388abec7
Markus Weissofner <57857702+markusweibofner@users.noreply.github.com>
2023-08-05 12:12:09 +0200

    Create link.py

---
+++ link.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import sys
+from urllib.request import urlopen
+
+channel_links = {
+    "CNN": "https://www.newslive.com/featured/cnn-live-free.html",
+    "FoxNews": "https://www.newslive.com/featured/fox-news.html",
+    "FoxBusiness": "https://www.newslive.com/business/fox-business-network-fbn.html",
+    "CBCNews": "https://www.newslive.com/canadian-news/cbc-news.html",
+    "AlJazeeraAmerica": "https://www.newslive.com/american/al-jazeera-america.html"
+}
+
+def get_channel_link(channel_name):
+    if channel_name in channel_links:
+        html = urlopen(channel_links[channel_name])
+        data = str(html.read())
+        start = data.find("file: ")
+        end = data.find(",autostart:")
+        link = str(data[start + 7:end - 1])
+        return link
+    else:
+        return None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 link.py <channel>")
+        sys.exit(1)
+
+    channel = sys.argv[1]
+    link = get_channel_link(channel)
+    if link:
+        print(link)
+    else:
+        print("Invalid channel name. Available channels: CNN, FoxNews, FoxBusiness, CBCNews, AlJazeeraAmerica")
